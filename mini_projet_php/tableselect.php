e8cb77d7b86188be7375bc46c37d0df3ba079fcd
qbaccon <qbaccon@hotmail.com>
2021-04-23 15:53:21 +0200

    update third part

--- mini_projet_php/tableselect.php
+++ mini_projet_php/tableselect.php
@@ -12,32 +12,5 @@
 		echo "</br>";
 	}
 
-	if(isset($_POST['1']))
-		calc(1);
-
-	if(isset($_POST['2']))
-		calc(2);
-
-	if(isset($_POST['3']))
-		calc(3);
-
-	if(isset($_POST['4']))
-		calc(4);
-
-	if(isset($_POST['5']))
-		calc(5);
-
-	if(isset($_POST['6']))
-		calc(6);
-
-	if(isset($_POST['7']))
-		calc(7);
-
-	if(isset($_POST['8']))
-		calc(8);
-
-	if(isset($_POST['9']))
-		calc(9);
-
-	if(isset($_POST['10']))
-		calc(10);
+	foreach ($_POST as $key => $valeur)
+		calc($key);
