16086930d12bf38531454d108e0f9087d4c57085
qbaccon <qbaccon@hotmail.com>
2021-04-28 12:42:34 +0200

    update form page

--- prj_ampoule/index.php
+++ prj_ampoule/index.php
@@ -4,10 +4,11 @@
 <html>
 	<head>
 		<meta charset="UTF-8"/>
-		<link rel="stylesheet" href="style/index.css"/>	
+		<link rel="stylesheet" href="style/index.css"/>
+		<link rel="icon" href="pctr/favicone_ampoule.jpg"/>
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<title>Gestionnaire</title>
+		<title>Gestionnaire-Formulaire d'intervention</title>
 	</head>
 	<body>
 		<section class="action_page">
